6857b2ccd6fca0332115667f9e09064b020ff0ae
abbad <ahm.abbad@gmail.com>
2015-10-13 15:04:17 -0400

    get largest palindromic number function

---
+++ largest_palindromic_number.py
@@ -0,0 +1,40 @@
+"""
+    This is a random question from project euler.
+    1. A palindromic number reads the same both ways.
+    The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 * 99.
+
+    Find the largest palindrome made from the product of two 3-digit numbers.
+
+"""
+
+
+def find_largest_palindrome():
+    array_list = list()
+    for x in range(1,999):
+        for y in range(1, 999):
+            array_list.append(x * y)
+
+    largest_palindrome = None
+    for x in array_list:
+        if x == array_list[0]:
+            largest_palindrome = array_list[0]
+
+        if is_palindrome(x) and x > largest_palindrome:
+            largest_palindrome = x
+
+    return largest_palindrome
+
+
+def is_palindrome(number):
+    str_number = str(number)
+    length = len(str_number) - 1
+    for char in str_number[:len(str_number)/2]:
+        if char == str_number[length]:
+            length -= 1
+        else:
+            return False
+
+    return True
+
+if __name__ == "__main__":
+    print find_largest_palindrome()
--- merge_sort.py
+++ merge_sort.py
@@ -1,47 +1,49 @@
 """ 
-	Merge sort function.
-	I learned and implemented this function during a Algorithms and Data structures course at my university.
-	I rewrote it for fun.
-	
-	Merge sort is a divide and conquer algorithm. It will make this list into a smaller lists by calling it self in recursion and then 
+    Merge sort function.
+    I learned and implemented this function during an Algorithms and Data structures course at my university.
+    I rewrote it for fun.
+
+    Merge sort is a divide and conquer algorithm. It will make this list into a smaller lists by calling it self in recursion and then
 """
 
 
 def merge_sort(merge_list): 
 
-	if len(merge_list) <= 1:
-		return merge_list
+    if len(merge_list) <= 1:
+        return merge_list
 
-	middle = len(merge_list) // 2 # This is floor division. why not to use a noraml division?
+    middle = len(merge_list) // 2 # This is floor division. why not to use a noraml division?
 
-	right_list = merge_sort(merge_list[middle:])
-	left_list =  merge_sort(merge_list[:middle])
+    right_list = merge_sort(merge_list[middle:])
+    left_list =  merge_sort(merge_list[:middle])
 
-	return merge(right_list, left_list)
-	
+    return merge(right_list, left_list)
+
 def merge(right_list, left_list):
-	result = []
-	r_idx = 0
-	l_idx = 0  
-	
-	while r_idx < len(right_list) and l_idx < len(left_list):
-		
-		if left_list[l_idx] < right_list[r_idx]: 
-			result.append(left_list[l_idx])
-			l_idx += 1
-		else:
-			result.append(right_list[r_idx])
-			r_idx += 1
-	
-	 
-	if len(right_list) > 0:
-		result.extend(right_list[r_idx:])
-		
-	if len(left_list) > 0:
-		result.extend(left_list[l_idx:])
-	
-	return result
-	
+    result = []
+    r_idx = 0
+    l_idx = 0
+
+    while r_idx < len(right_list) and l_idx < len(left_list):
+
+        if left_list[l_idx] < right_list[r_idx]:
+            result.append(left_list[l_idx])
+            l_idx += 1
+        else:
+            result.append(right_list[r_idx])
+            r_idx += 1
+
+
+    if len(right_list) > 0:
+        result.extend(right_list[r_idx:])
+
+    if len(left_list) > 0:
+        result.extend(left_list[l_idx:])
+
+    return result
+
 if __name__ == "__main__":
-	
-	print merge_sort([5,4,3,2,1])
+
+    # print merge_sort([2,1,3,5,-1])
+
+	print merge([3,2,1], [7, 6, 5])
