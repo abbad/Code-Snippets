b42ff961572802b9c8203e4456c2ada5fb1ef0c2
Abbad <ahm.abbad@gmail.com>
2018-04-17 13:58:35 -0400

    refactored compress_string_func

--- python_snippets/compress_string.py
+++ python_snippets/compress_string.py
@@ -8,40 +8,23 @@ replaced by a count of the repetitions.
  
  
 def compress_string(string):
-	"""
-		Function that takes a string and returns a copy of the string with consecutive repeating characters.   
-	""" 
-	
-	compressed_string = ""
-	string_pointer = 0
-	char_count = 1
-	
-	if len(string) == 0:
-		return ""
-	
-	while (string_pointer != len(string)):
-		# Add character to compressed_string.
-		compressed_string += string[string_pointer]
-		
-		# Increment string pointer.
-		string_pointer += 1
-		
-		while (string_pointer < len(string)):
-			# Check next character with the last one added to compressed string array. 
-			if (compressed_string[-1] == string[string_pointer]):
-				char_count += 1
-				string_pointer += 1
-				# This if statement is written to handle the last repetition. 
-				if len(string) == string_pointer: 
-					compressed_string += str(char_count)
-			else: 
-				# This is used to handle not adding the 1 when the string does not contain repeats. 
-				if char_count == 1:
-					break
-				compressed_string += str(char_count)
-				char_count = 1
-				break
-	
-	return compressed_string
-		
-		
+    """
+    :param string:
+    :return:
+    """
+    result = string[:1]
+    count = 1
+
+    for index in range(len(string) - 1):
+        if string[index] == string[index + 1]:
+            count += 1
+        else:
+            if count > 1:
+                result += str(count)
+            result += string[index + 1]
+            count = 1
+
+    if count > 1:
+        result += str(count)
+
+    return result
