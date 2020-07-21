6485f2b78c1daab704f22933c359ae1145a093e7
Ahmad Abbad <1731803+abbad@users.noreply.github.com>
2020-07-20 22:14:07 -0400

    Update lru.py

--- python_snippets/lru.py
+++ python_snippets/lru.py
@@ -1,17 +1,100 @@
 
 class Node(object):
+    def __init__(self, key, value, previous=None, next_node=None):
+        self.value = value 
+        self.previous = previous 
+        self.next_node = next_node
+        self.key = key
 
-  def __init__(self, key, value, previous=None, next_node=None):
-      self.value = value 
-      self.previous = previous 
-      self.next_node = next_node
-      self.key = key
+    def __repr__(self):
+        return f"current: { str(self.value)}"
+    
+# Solution 1: Using a doubly linked list.
+class LRUCache1:
+
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.cache = {}
+        self.tail = None
+        self.head = None
+
+    def put(self, key, value):
+        """
+        if there is an empty space (capacity is not full),
+        add element to the cache and also add it to the doubly linked list.
+        every time a new item is added the head changes to point to it. 
+
+        if the cache was full -> 
+        evict element that is at the tail. and add the new element to the head of the doubly linked list and also the cache. 
+        """
+        node = Node(key, value)
+
+        if len(self.cache) < self.capacity:
+            # The addition section to handle adding to the doubly linked list. 
+            if not self.head:
+                self.head = node
+                self.tail = node
+            else: 
+                previous_head = self.head 
+
+                node.previous = previous_head
+                previous_head.next_node = node
+
+                self.head = node
+                
+            self.cache[key] = node
+        else:   
+            # evict the tail and delete value from cache
+            previous_tail = self.tail 
+            self.tail = previous_tail.next_node
+            
+            del self.cache[previous_tail.key]
+            
+            # add new element to teh cache and update the head.
+            previous_head = self.head 
+            
+            node.previous = previous_head
+            previous_head.next_node = node
+            self.head = node
+
+            self.cache[key] = node
+
+    def get(self, key):
+        """
+            if element does not exist return -1. 
+            if element is inside the cache, update the doubly linked list to have the element be at the head. 
+        """
+    
+        if not self.cache.get(key):
+            return -1 
+
+        node = self.cache[key]
+
+        previous_head = self.head 
+
+        if node == self.tail:
+            self.tail = self.tail.next_node
+
+        node.previous = previous_head
+        node.next_node = None
+        previous_head.next_node = node
+
+        self.head = node
+
+        return node.value
+
+    def print_values(self):
+        pointer_node = self.tail  
+    
+        while (pointer_node):  
+            print(pointer_node)      
+
+            pointer_node = pointer_node.next_node
+        
+        print('end of printing')
 
 
-  def __repr__(self):
-      return f"current: { str(self.value)}"
-
-ru = LRUCache2(3)
+lru = LRUCache1(3)
 lru.put(1, 101)
 lru.put(2, 102)
 lru.put(3, 103)
@@ -34,6 +117,9 @@ assert lru.get(3) == 103
 assert lru.get(1) == -1
 lru.print_values()
 
+assert lru.tail.value == 104, lru.tail.value
+assert lru.head.value == 103
+# 
 lru.put(4, 104)
 lru.print_values()
 
@@ -52,122 +138,41 @@ assert lru.get(4) == 104
 assert lru.get(5) == 105
 
 print("Success")
-    
-# Solution 1: Using a doubly linked list.
-class LRUCache3:
-
-  def __init__(self, capacity):
-    self.capacity = capacity
-    self.cache = {}
-    self.tail = None
-    self.head = None
-
-  def put(self, key, value):
-    """
-      if there is an empty space (capacity is not full),
-      add element to the cache and also add it to the doubly linked list.
-      every time a new item is added the head changes to point to it. 
-
-      if the cache was full -> 
-      evict element that is at the tail. and add the new element to the head of the doubly linked list and also the cache. 
-    """
-    node = Node(key, value)
-
-    if len(self.cache) < self.capacity:
-      # The addition section to handle adding to the doubly linked list. 
-      if not self.head:
-        self.head = node
-        self.tail = node
-      else: 
-        previous_head = self.head 
-        
-        node.previous = previous_head
-        previous_head.next_node = node
-        
-        self.head = node
-          
-      self.cache[key] = node
-    else:
-      # evict the tail and delete value from cache
-      previous_tail = self.tail 
-      self.tail = previous_tail.next_node
-      
-      del self.cache[previous_tail.key]
-      
-      # add new element to teh cache and update the head.
-      previous_head = self.head 
-        
-      node.previous = previous_head
-      previous_head.next_node = node
-      self.head = node
-
-      self.cache[key] = node
-
-  def get(self, key):
-    """
-      if element does not exist return -1. 
-      if element is inside the cache, update the doubly linked list to have the element be at the head. 
-    """
-    
-    if not self.cache.get(key):
-      return -1 
-    
-    node = self.cache[key]
-
-    previous_head = self.head 
-    
-    if node == self.tail:
-      self.tail = self.tail.next_node
-
-    node.previous = previous_head
-    node.next_node = None
-    previous_head.next_node = node
-    
-    self.head = node
-
-    return node.value
-
-  def print_values(self):
-    pointer_node = self.tail  
-    
-    while (pointer_node):  
-      print(pointer_node)      
-
-      pointer_node = pointer_node.next_node
-    
-    print('end of printing')
 
 # Solution 2 Using an ordered Dictionary, all methods used from the ordered dictionary are o(1),
 from collections import OrderedDict
 
 class LRUCache2:
 
-  def __init__(self, capacity):
-      self.capacity = capacity
-      self.cache = OrderedDict()
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.cache = OrderedDict()
   
-  def put(self, key, value):
-      # ordered dictionary can have the same key twice, therefore we delete.
-      if key in self.cache:
-          del self.cache[key]
+    def put(self, key, value):
+        # ordered dictionary can have the same key twice, therefore we delete.
+        if key in self.cache:
+            del self.cache[key]
 
-      if len(self.cache.keys()) < self.capacity:
-          self.cache[key] = value
-      else: ## handle eviction
-          self.cache.popitem(last=False) # o(1)
-          self.cache[key] = value
+        if len(self.cache.keys()) < self.capacity:
+            self.cache[key] = value
+        else: ## handle eviction
+            self.cache.popitem(last=False) # o(1)
+            self.cache[key] = value
 
-  def get(self, key):
-      value = self.cache.get(key, None)
+    def get(self, key):
+        value = self.cache.get(key, None)
 
-      if not value:
-          return -1
+        if not value:
+            return -1
 
-      self.cache.move_to_end(key)
+        self.cache.move_to_end(key)
 
-      return value
+        return value
 
-lru = LRUCache3(3)
+    def print_values(self):
+        print(self.cache)
+
+lru = LRUCache2(3)
 lru.put(1, 101)
 lru.put(2, 102)
 lru.put(3, 103)
@@ -190,9 +195,6 @@ assert lru.get(3) == 103
 assert lru.get(1) == -1
 lru.print_values()
 
-assert lru.tail.value == 104
-assert lru.head.value == 103
-# 
 lru.put(4, 104)
 lru.print_values()
 
